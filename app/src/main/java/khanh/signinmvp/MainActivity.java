fb1478e7e7aca0c0d94a94e266663849af279966
QUOC-KHANH\saakh <saakhaka@gmail.com>
2019-01-17 23:23:57 +0700

    Interface này sẽ có nhiệm vụ giao tiếp giữa View và Presenter

--- app/src/main/java/khanh/signinmvp/MainActivity.java
+++ app/src/main/java/khanh/signinmvp/MainActivity.java
@@ -1,16 +1,27 @@
 package khanh.signinmvp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import khanh.signinmvp.Interface.SignInContract;
+
+public class MainActivity extends AppCompatActivity implements SignInContract.View{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("Khanh", "Text");
+
+    }
+
+    @Override
+    public void signInSuccess() {
+
+    }
+
+    @Override
+    public void signInFailure(String error) {
+
     }
 }
--- app/src/main/java/khanh/signinmvp/Presenter/SignInPresenter.java
+++ app/src/main/java/khanh/signinmvp/Presenter/SignInPresenter.java
@@ -4,8 +4,17 @@ import khanh.signinmvp.Interface.SignInContract;
 
 public class SignInPresenter implements SignInContract.Presenter {
 
+    private SignInContract.View mView;
+
+    public SignInPresenter(SignInContract.View mView) {
+        this.mView = mView;
+    }
+
     @Override
     public void handleSignIn(String userName, String passWord) {
-
+        if (userName.equals("devk") && passWord.equals("123456")){
+            mView.signInSuccess();
+            return;
+        }
     }
 }
